ea4b320797f4db3ead13efbc97831c5dd901cdb2
Bing <bing@Bings-MacBook-Pro.local>
2021-05-23 20:36:02 +0900

    adds event detail component page

---
+++ miniprogram/pages/components/eventInfo/index.ts
@@ -0,0 +1,26 @@
+// pages/components/eventInfo.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+    event: {
+      type: Object,
+      value: undefined
+    }
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
